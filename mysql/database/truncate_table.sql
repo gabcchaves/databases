5154b0967369a9cb00b22a38f2980afc05ed5e2c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-21 19:10:53 -0300

    Truncate tables.

---
+++ mysql/database/truncate_table.sql
@@ -0,0 +1,10 @@
+/* Truncate table, i.e., delete all its data, but not itself. */
+TRUNCATE TABLE foo;
+
+TRUNCATE TABLE bar;
+
+TRUNCATE TABLE baz;
+
+TRUNCATE TABLE foobar;
+
+TRUNCATE TABLE foobaz;
